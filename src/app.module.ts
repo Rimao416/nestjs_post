8fb9a80d3d528a6fdc9e8847a9cb8600af4a9d74
Rimao416 <kayumbaomari27@gmail.com>
2025-08-22 13:58:05 +0200

    :construction: Initialisation du nouveau projet NestJs du Blog

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+
+@Module({
+  imports: [UsersModule, PostsModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
+})
+export class AppModule {}
